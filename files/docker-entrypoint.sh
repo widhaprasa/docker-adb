04fdf804511b6d8c67d1a873522ba4e8901bc646
widhaprasa <widhaprasa@gmail.com>
2023-03-23 13:28:04 +0700

    Update docker-entrypoint for sshd

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo -n adb:$SSHPASS | chpasswd
 
 # Start ssh as daemon service
 ssh-keygen -A
-/etc/init.d/sshd restart
+/usr/sbin/sshd
 
 # Start adbportforward as entrypoint
 java -jar adbportforward.jar server adblocation=/opt/platform-tools
